8fa59dffe26766f9a6207b7f4ef6baebd7ee6f04
matthewglassman <matthewglassman78@gmail.com>
2017-04-06 12:26:38 -0400

    Able to scrape website for an articles title and link successfully.

--- server.js
+++ server.js
@@ -1,9 +1,35 @@
 var express = require('express');
-var app = express();
+
 var bodyParser = require('body-parser');
+var request = require('request');
+var cheerio = require('cheerio');
 
-var PORT = process.env.PORT || 8080;
+// var app = express();
 
-app.listen(PORT, function(){
-	console.log("The app is listening on port" + PORT);
-});
+// var PORT = process.env.PORT || 8080;
+
+var url = "http://www.newsobserver.com/news/local/"; //website to scrape
+
+	request(url, function(err, resp, body){
+		var $ = cheerio.load(body);
+
+		var result = [];
+
+		$("article").each(function(i, element){
+
+			var title = $(this).find("h4").find("a").text();
+
+			var link = $(this).find("h4").find("a").attr("href");
+
+			result.push({
+				title: title,
+				link: link,
+			});
+		});
+		console.log(result);
+
+	});
+
+// app.listen(PORT, function(){
+// 	console.log("The app is listening on port" + PORT);
+// });
