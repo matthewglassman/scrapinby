637299cb02f97698f647aa806d9b10fbf49f556b
matthewglassman <matthewglassman78@gmail.com>
2017-04-06 16:00:54 -0400

    Working on mongoose model creation

---
+++ models/newsandreviews.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema; //set the mongoose Schema method to a variable called Schema
+
+var NewsAndReviewsSchema = new Schema({
+	title: String,
+	link: String,
+	user: [{name: String}],
+	comments: [{body: String, date: Date}]
+
+});
+
+module.export = mongoose.model();
