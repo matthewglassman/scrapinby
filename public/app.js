b0d89f22d4d82a556230b8eafdfd856d5cfe3ee6
matthewglassman <matthewglassman78@gmail.com>
2017-04-13 15:40:34 -0400

    Still needing to add functionality to write notes

--- public/app.js
+++ public/app.js
@@ -1,1 +1,24 @@
 //Functionality for pushing items to the database and deleting them from the database.
+$("#scraperbutton").on("click", function(){
+	$.get("/articles", function(news){});
+});
+
+$("#savedarticles").on("click", function(){
+	$.get("/saved")
+});
+
+$(document).on("click", "#save", function() {
+	var newsId = $(this).attr("data-id");
+
+	$.ajax({
+		method: "POST",
+		url: "/articles/" + newsId,
+		data: {
+			title: $("#newstitle").val(),
+			link: $("#newslink").val()
+		}
+	})
+	.done(function(data){
+		console.log(data);
+	});
+});
