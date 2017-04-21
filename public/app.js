6f30aa8e8590ac890a36c56dd64e44a00b17758e
matthewglassman <matthewglassmn78@gmail.com>
2017-04-21 15:52:57 -0400

    Redoing App.JS

--- public/app.js
+++ public/app.js
@@ -1,43 +1,61 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 	
-	function saveNews(scrapedNews){
-		$.post("/api/savingnews", scrapedNews, function(){
-			window.location.href = "/savednews"
-		});
-	},
+// 	function saveNews(scrapedNews){
+// 		$.post("/api/savingnews", scrapedNews, function(){
+// 			window.location.href = "/savednews"
+// 		});
+// 	},
 
-	function saveComments(newComment){
-		$.post("/api/comments/:id", newComment, function(){
-			window.location.href = "/api/savedcomments" + newComment.newsId;
-		});
-	},
+// 	function saveComments(newComment){
+// 		$.post("/api/comments/:id", newComment, function(){
+// 			window.location.href = "/api/savedcomments" + newComment.newsId;
+// 		});
+// 	},
 
-	$('#')
+// 	$('#')
 
-})
+// })
 
 
 //Functionality for pushing items to the database and deleting them from the database.
-$("#scraperbutton").on("click", function(){
-	$.get("/scrape", function(data){});
-});
+// $("#scraperbutton").on("click", function(){
+// 	$.get("/scrape", function(data){});
+// });
 
-$("#savedarticles").on("click", function(){
-	$.get("/saved")
-});
+// $("#savedarticles").on("click", function(){
+// 	$.get("/saved")
+// });
 
+//save an article to database
 $(document).on("click", "#save", function() {
 	var newsId = $(this).attr("data-id");
 
 	$.ajax({
 		method: "POST",
-		url: "/articles/" + newsId,
+		url: "/api/savednews/" + newsId,
 		data: {
 			title: $("#newstitle").val(),
 			link: $("#newslink").val()
 		}
-	})
-	.done(function(data){
+	}).done(function(data){
 		console.log(data);
 	});
 });
+
+//delete an article from database
+$(document).on("click", "#remove", function(){
+	var newsId = $(this).attr("data-id");
+
+	$.ajax({
+		method: "POST",
+		url: "/api/removenews/" + newsId,
+		data: {
+			title: $("#newstitle").val(),
+			link: $("newslink").val()
+		}
+	}).done(function(data){
+		console.log(data);
+	});
+});
+
+//Save a Comment to an article
